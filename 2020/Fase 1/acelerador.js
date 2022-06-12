01ce37a13211ce988f3d70e343bc7eb76dc7f67b
Lucas Manoel <89367719+lucasarruda12@users.noreply.github.com>
2022-06-12 17:57:53 -0300

    Update acelerador.js

--- 2020/Fase 1/acelerador.js
+++ 2020/Fase 1/acelerador.js
@@ -1,3 +1,10 @@
+// Pontuação	100 / 100
+// Linguagem	Javascript
+// Limite de tempo permitido	1,4 s
+// Limite de memória permitido	1.0 GB
+// Máximo tempo usado	0,591 s
+// Máxima memória usada	12.5 MB
+
 var distancia
 
 scanf("%d", "distancia")
